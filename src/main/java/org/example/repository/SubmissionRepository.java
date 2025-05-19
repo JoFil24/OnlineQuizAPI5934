f0200951046083c38b49f8bbe208c94b61e17d98
Treecky69 <filipthedank2407@gmail.com>
2025-05-19 21:30:22 +0200

    Made the repositories

--- src/main/java/org/example/repository/SubmissionRepository.java
+++ src/main/java/org/example/repository/SubmissionRepository.java
@@ -1,7 +1,6 @@
 package org.example.repository;
 
 import org.example.model.Submission;
-import org.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
