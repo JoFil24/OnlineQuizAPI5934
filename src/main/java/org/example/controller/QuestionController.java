3a5351fc6d338b500e87b8c30647b4efd457f397
Treecky69 <filipthedank2407@gmail.com>
2025-05-20 09:34:58 +0200

    Did service for question

---
+++ src/main/java/org/example/controller/QuestionController.java
@@ -0,0 +1,13 @@
+package org.example.controller;
+
+import org.example.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/questions")
+public class QuestionController {
+    @Autowired
+    private QuestionService questionService;
+}
---
+++ src/main/java/org/example/service/QuestionService.java
@@ -0,0 +1,39 @@
+package org.example.service;
+
+import org.example.model.Question;
+import org.example.repository.QuestionRepository;
+import org.example.repository.QuizRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class QuestionService {
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private QuizRepository quizRepository;
+
+    public List<Question> getAllQuestions(){
+        return questionRepository.findAll();
+    }
+
+    public Question getQuestionById(Long id){
+        return questionRepository.findById(id).orElseThrow(() -> new RuntimeException("Question not found"));
+    }
+
+    public Question saveQuestion(Question question){
+        return questionRepository.save(question);
+    }
+
+    public void deleteQuestion(Long id){
+        questionRepository.deleteById(id);
+    }
+
+    public List<Question> getQuestionByQuizId(Long quizId){
+        return questionRepository.findByQuizId(quizId).orElseThrow(() -> new RuntimeException("Question not found"));
+    }
+}
